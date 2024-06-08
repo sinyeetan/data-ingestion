6a3a55f9e93f7213b584a9af0e50e6a8b0546c91
sinyeetan <105202118+sinyeetan@users.noreply.github.com>
2024-06-08 12:50:39 +0800

    Update settings.py

--- Worker/settings.py
+++ Worker/settings.py
@@ -4,13 +4,13 @@ HUB_HOST = "localhost"
 HUB_PORT = 1883
 HUB_TOPIC = "mlops/#"
 
-LAKE_HOST = os.getenv('LAKE_HOST',"192.168.122.75")
+LAKE_HOST = os.getenv('LAKE_HOST')
 MINIO_PORT = os.getenv('MINIO_PORT',"9000")
 MINIO_BUCKET = os.getenv('MINIO_BUCKET',"ml-ops")
-MINIO_ACCESS = os.getenv('MINIO_ACCESS',"QmBvktoBhb5hxfqj")
-MINIO_SECRET = os.getenv('MINIO_SECRET',"F1wG5vpmczDeTVjbz5LGnMA0b8D782Et")
+MINIO_ACCESS = os.getenv('MINIO_ACCESS')
+MINIO_SECRET = os.getenv('MINIO_SECRET')
 
-DB_USER = os.getenv('DB_USER',"data_uploader")
-DB_PW = os.getenv('DB_PW',"pa33w0rd")
+DB_USER = os.getenv('DB_USER')
+DB_PW = os.getenv('DB_PW')
 DB_PORT = os.getenv('DB_PORT',"5432")
 DB_NAME = os.getenv('DB_NAME',"mlops")
